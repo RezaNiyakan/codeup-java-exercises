0545bd2091bddb7194d7a98836584981bf47fda5
Reza Niyakan <RezaNiyakan@gmail.com>
2021-12-15 10:06:46 -0600

    Completed the syntax,types and variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,87 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+//        syntax data types
+//        1.
+//        int myFavoriteNumber = 33;
+//            System.out.println(myFavoriteNumber);
+
+//        2.
+//        String myString = "Hello Codeup";
+//            System.out.println(myString);
+
+//        3.
+//        myString = 'C';
+
+        //4.
+//        myString = 3.14159;
+
+        //5.
+//        long myNumber;
+//            System.out.println(myNumber);
+
+        //6.
+//        myNumber = 3.14;
+
+        //7.
+//        myNumber = 123L;
+//        System.out.println(myNumber);
+
+        //8.
+//        myNumber = 123;
+//        System.out.println(myNumber);
+
+        //9.
+        // We can add our "f" literal notation
+        float myNumber = 3.14f;
+        // or we can cast to a float
+//        float myNumber = (float)3.14;
+        System.out.println(myNumber);
+
+        // 10.
+//        int x = 5;
+//            System.out.println(x++);
+//            System.out.println(x);
+
+//        int x = 5;
+//            System.out.println(++x);
+//            System.out.println(x);
+//
+            //11.
+//        String class = "Hello";
+
+            //12.
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        #13
+//        int x = 4;
+//        x = x + 5;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        y *= x;
+        //System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+
+//        The code below will cause a compile error
+//                byte biggerInt = 129;
+//        System.out.println();
+
+
     }
 }
+
